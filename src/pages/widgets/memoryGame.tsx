d5bcf8e62bd14e953da48bc7b451428f76d0e042
Vince Maina <vchapandrews@gmail.com>
2023-04-26 14:16:54 +0100

    Added another test widget

---
+++ src/pages/widgets/memoryGame.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div className={"min-vh-100 bg-success d-flex justify-content-center"}>
+            <div className={"m-auto"}>
+                <h1>Memory game</h1>
+            </div>
+        </div>
+    )
+}
