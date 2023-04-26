b72219b383513b70f749e04112dcb27cbf2c2103
Vince Maina <vchapandrews@gmail.com>
2023-04-26 13:16:05 +0100

    Added another test widget to demonstrate new widgetLoader in action

---
+++ src/pages/widgets/test2.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div className={"min-vh-100 bg-info d-flex justify-content-center"}>
+            <div className={"m-auto"}>
+                <h1>Another</h1>
+            </div>
+        </div>
+    )
+}
