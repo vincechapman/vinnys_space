041333125f85c64dd72c4f6de496ffb5596f890d
Vince Maina <vchapandrews@gmail.com>
2023-04-26 17:57:46 +0100

    Updated to trigger new vercel deploy

--- src/pages/widgets/test/[currentIndex].tsx
+++ src/pages/widgets/test/[currentIndex].tsx
@@ -1,12 +1,8 @@
-import {useRouter} from "next/router";
-
 export default function Home() {
-    const router = useRouter();
-
     return (
         <div className={"min-vh-100 bg-danger d-flex justify-content-center"}>
             <div className={"m-auto"}>
-                <h1>Test {router.query.currentIndex}</h1>
+                <h1>First slide</h1>
             </div>
         </div>
     )
